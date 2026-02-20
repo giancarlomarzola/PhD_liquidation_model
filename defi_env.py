576ef1f85c8288ac4c3be3262b0cc4597209ff30
Giancarlo Marzola <giancarlomarzola00@gmail.com>
2026-02-20 21:26:15 +0000

    At the start of trying to implement Wallet total_collateral etc to allow for better checks when borrowing or withdrawing

--- defi_env.py
+++ defi_env.py
@@ -44,7 +44,7 @@ class Wallet:
         self,
         env: DefiEnv,
         name: str,
-        balances: dict[str, float] | None = None,
+        balances: dict[Token, float] | None = None,
         is_liquidator: bool=False
     ):
         # Add Wallet to defi environment
@@ -63,6 +63,32 @@ class Wallet:
     # Properties: allowed_collateral || wealth, borrow_value, supply_value, max_borrowable_value, 
     # Methods: initialise_transaction, get_liquidation_candidates 
     # Maybe: helper method to request supply_withdraw from wallet, instead of calling it from pool
+
+    def __str__(self) -> str:
+        return(
+            f"{self.name} Wallet State\n"
+            + f"\tBalances: {self.balances}\n"
+            + f"\tIs Liquidator: {self.is_liquidator}"
+        )
+    
+    # @property
+    # def total_collateral(self):
+    #     total_collateral = 0
+    #     for symbol in self.balances:
+    #         # collateral = tokens * max_ltv * price
+    #         total_collateral += self.balances[symbol] * defi_env.lending_pools.get(symbol).max_ltv * defi_env.prices[symbol]
+    #     return(total_collateral)
+    
+    @property
+    def total_collateral(self):
+        total = 0.0
+
+        for token, amount in self.balances.items():
+            if isinstance(token, aToken):
+                pool = token.pool
+                total += amount * pool.underlying_token.price * pool.max_ltv
+
+        return total
 
 
 class Token:
@@ -90,8 +116,8 @@ class Token:
         assert amount > 0
         self.total_supply += amount
         if self.symbol not in wallet.balances:
-            wallet.balances[self.symbol] = 0.0
-        wallet.balances[self.symbol] += amount
+            wallet.balances[self] = 0.0
+        wallet.balances[self] += amount
 
     def burn(self, wallet: Wallet, amount: float):
         assert amount > 0
@@ -101,14 +127,26 @@ class Token:
         wallet.balances[self.symbol] -= amount
 
 
+class aToken(Token):
+    def __init__(self, env, symbol, pool):
+        super().__init__(env, symbol)
+        self.pool = pool
+
+
+class vToken(Token):
+    def __init__(self, env, symbol, pool):
+        super().__init__(env, symbol)
+        self.pool = pool
+
+
 class LendingPool:
     def __init__(
         self,
         env: DefiEnv,
         underlying_token: Token,
-        total_supplied: float,
+        total_supplied: float, # TODO: Should these be replaced by only using aToken and vToken instead?
         total_borrowed: float,
-        interest_rate: float,
+        interest_rate: float, # TODO: change to borrow and supply rate
         reserve_rate: float,
         max_ltv: float,                 # how much of supply can be used as collateral
         liquidation_bonus: float,       # reward for liquidator (aka liquidation_penalty)
@@ -118,16 +156,14 @@ class LendingPool:
         v_token: Token | None = None,
     ):
 
-        # Create a_token and v_token for underlying
-        if a_token is None:
-            a_token = Token(env, f"a_{underlying_token.symbol}")
-        if v_token is None:
-            v_token = Token(env, f"v_{underlying_token.symbol}")
-
+        # Add Lending Pool to defi environment
+        assert underlying_token.symbol not in env.lending_pools, f"Lending Pool {underlying_token.symbol} exists"
         self.env = env
+        self.env.lending_pools[underlying_token.symbol] = self
+
         self.underlying_token = underlying_token
-        self.a_token = a_token
-        self.v_token = v_token
+        self.a_token = aToken(self.env, f"a_{underlying_token.symbol}", self)
+        self.v_token = vToken(self.env, f"v_{underlying_token.symbol}", self)
         self.total_supplied = total_supplied
         self.total_borrowed = total_borrowed
         self.interest_rate = interest_rate
@@ -140,67 +176,62 @@ class LendingPool:
     # Properties: bad_debt, utilisation_rate
     # Methods: supply, withdraw, borrow, repay, update_borrow_lend_rates, accrue_interest, liquidate
 
-    def supply(self, wallet: Wallet, amount: float):
-        # Check requirements
+    def __str__(self) -> str:
+        return(
+            f"{self.underlying_token.symbol} Pool State at block {self.env.blocknumber}\n"
+            + f"\tTotal Supplied: {self.total_supplied:.2f}\t"
+            + f"\tTotal Borrowed: {self.total_borrowed:.2f}\n"
+            + f"\ta_token Supply: {self.a_token.total_supply:.2f}\t"
+            + f"\tv_token Supply: {self.v_token.total_supply:.2f}\n"
+            + f"\tInterest Rate: {self.interest_rate*100:.2f}%\n"
+            + f"\tLiquidation Parameters: \t max_ltv = {self.max_ltv*100:.2f}%, liquidation_bonus = {self.liquidation_bonus*100:.2f}%, "
+            + f"liquidation_threshold = {self.liquidation_threshold*100:.2f}%, closing_factor = {self.closing_factor*100:.2f}%"
+        )
+    
+    @property
+    def available_liquidity(self):
+        return(self.total_supplied - self.total_borrowed)
+    
+    @property
+    def utilisation_rate(self):
+        return(self.total_borrowed / self.total_supplied)
+
+    def _transfer(self, wallet: Wallet, token: Token, amount: float, from_wallet: bool):
         assert amount > 0, "Amount must be positive"
-        balance = wallet.balances.get(self.underlying_token.symbol, 0.0)
-        assert balance >= amount, f"Wallet does not have enough {self.symbol} to supply"
+        wallet_balance = wallet.balances.get(token.symbol, 0.0)
+        if from_wallet is True:
+            assert wallet_balance >= amount, f"Wallet does not have enough {token.symbol}"
+            wallet.balances[token.symbol] -= amount
+        else:
+            wallet.balances[token.symbol] += amount
 
-        # take token amount from wallet
-        wallet.balances[self.underlying_token.symbol] -= amount
+    def _update_pool(self, attr: str, amount: float, add: bool = True):
+        if add:
+            setattr(self, attr, getattr(self, attr) + amount)
+        else:
+            setattr(self, attr, getattr(self, attr) - amount)
 
-        # mint a_token to wallet
+    def supply(self, wallet: Wallet, amount: float):
+        self._transfer(wallet, self.underlying_token, amount, from_wallet=True)
         self.a_token.mint(wallet, amount)
-
-        # add token amount to total_supplied
-        self.total_supplied += amount
+        self._update_pool("total_supplied", amount, add=True)
 
     def withdraw(self, wallet: Wallet, amount: float):
-        # check requirements
-        assert amount > 0, "Amount must be positive"
-        balance = wallet.balances.get(self.a_token.symbol, 0.0)
-        assert balance - amount >= 0, f"Wallet does not have enough {self.a_token.symbol} to withdraw"
-        # TODO: Check that withdrawal does not reduce LTV too much
-
-        # return token amount to wallet
-        wallet.balances[self.underlying_token.symbol] += amount
-
-        # take a_token from wallet
-        self.a_token.burn(wallet, amount)
-
-        # remove token amount from total_supplied
-        self.total_supplied -= amount
+        assert self.available_liquidity >= amount, "Not enough liquidity in pool to withdraw"
+        self._transfer(wallet, self.a_token, amount, from_wallet=True)
+        self._transfer(wallet, self.underlying_token, amount, from_wallet=False)
+        self._update_pool("total_supplied", amount, add=False)
 
     def borrow(self, wallet: Wallet, amount: float):
-        # check requirements
-        assert amount > 0, "Amount must be positive"
-        available_liquidity = self.total_supplied - self.total_borrowed
-        assert available_liquidity >= amount, "Not enough liquidity in pool to borrow"
-        # TODO: check how much the wallet is allowed to borrow
-
-        # Take token amount from wallet
-        wallet.balances[self.underlying_token.symbol] -= amount
-
-        # mint v_token to wallet
+        assert self.available_liquidity >= amount, "Not enough liquidity in pool to borrow" #TODO: should borrowing be restricted before utilisation=100%?
+        self._transfer(wallet, self.underlying_token, amount, True)
         self.v_token.mint(wallet, amount)
+        self._update_pool("total_borrowed", amount, add=True)
 
-        # Add token amount to pool's total_borrowed
-        self.total_borrowed += amount
-    
     def repay(self, wallet: Wallet, amount: float):
-        # check requirements
-        assert amount > 0, "Amount must be positive"
-        balance = wallet.balances.get(self.v_token.symbol, 0.0)
-        assert balance - amount >= 0, f"Wallet does not have enough {self.v_token.symbol} to repay"
-
-        # return token amount to wallet
-        wallet.balances[self.underlying_token.symbol] += amount
-
-        # take v_token from wallet
-        self.v_token.burn(wallet, amount)
-
-        # remove token amount from total_borrowed
-        self.total_borrowed -= amount
+        self._transfer(wallet, self.v_token, amount, from_wallet=True)
+        self._transfer(wallet, self.underlying_token, amount, from_wallet=False)
+        self._update_pool("total_borrowed", amount, add=False)
 
 
 
@@ -216,38 +247,44 @@ liquidation_bonus = 0.05
 closing_factor = 0.5
 
 
-defi_env = DefiEnv()
+defi_env = DefiEnv(prices={"usdc":1, "wbtc":50000})
 
 usdc = Token(defi_env, "usdc")
 wbtc = Token(defi_env, "wbtc")
 
-usdc_pool = LendingPool(defi_env, usdc, 0, 0, 0.03, 0.001, max_ltv, liquidation_bonus, liquidation_threshold, closing_factor)
-wbtc_pool = LendingPool(defi_env, wbtc, 0, 0, 0.003, 0.0001, max_ltv, liquidation_bonus, liquidation_threshold, closing_factor)
+usdc_pool = LendingPool(defi_env, usdc, 0, 0, 0.03, 0.2, max_ltv, liquidation_bonus, liquidation_threshold, closing_factor)
+wbtc_pool = LendingPool(defi_env, wbtc, 0, 0, 0.003, 0.2, max_ltv, liquidation_bonus, liquidation_threshold, closing_factor)
 
 Alice = Wallet(defi_env, "alice")
 
 usdc.mint(Alice, 50_000)
 wbtc.mint(Alice, 0.1)
 
-print("Alice balances: ", Alice.balances)
-print("USDC pool - Supplied: ", usdc_pool.total_supplied, "Borrowed: ", usdc_pool.total_borrowed)
+print("\nInitial state")
+print(Alice)
+print(usdc_pool)
 
+print("\nSupply 20k")
 usdc_pool.supply(Alice, 20_000)
-print("Alice balances: ", Alice.balances)
-print("USDC pool - Supplied: ", usdc_pool.total_supplied, "Borrowed: ", usdc_pool.total_borrowed)
+print(Alice)
+print(usdc_pool)
 
+print("\nWithdraw 5k")
 usdc_pool.withdraw(Alice, 5_000)
-print("Alice balances: ", Alice.balances)
-print("USDC pool - Supplied: ", usdc_pool.total_supplied, "Borrowed: ", usdc_pool.total_borrowed)
+print(Alice)
+print(usdc_pool)
 
-usdc_pool.borrow(Alice, 7_000)
-print("Alice balances: ", Alice.balances)
-print("USDC pool - Supplied: ", usdc_pool.total_supplied, "Borrowed: ", usdc_pool.total_borrowed)
+print("\nBorrow 10k")
+usdc_pool.borrow(Alice, 10_000)
+print(Alice)
+print(usdc_pool)
 
-usdc_pool.repay(Alice, 7_000)
-print("Alice balances: ", Alice.balances)
-print("USDC pool - Supplied: ", usdc_pool.total_supplied, "Borrowed: ", usdc_pool.total_borrowed)
+print("\nRepay 10k")
+usdc_pool.repay(Alice, 10_000)
+print(Alice)
+print(usdc_pool)
 
+print("\nWithdraw full a_usdc amount")
 usdc_pool.withdraw(Alice, Alice.balances.get("a_usdc"))
-print("Alice balances: ", Alice.balances)
-print("USDC pool - Supplied: ", usdc_pool.total_supplied, "Borrowed: ", usdc_pool.total_borrowed)
+print(Alice)
+print(usdc_pool)
