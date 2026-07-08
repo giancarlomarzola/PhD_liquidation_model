b0545f495640a590f92b59bad390e3b87d781ad9
Giancarlo Marzola <giancarlomarzola00@gmail.com>
2026-07-08 16:21:33 +0100

    Minor comments

--- scripts/simulation.py
+++ scripts/simulation.py
@@ -1,1 +1,11 @@
 # Placeholder for now
+
+# create n wallets
+# get initial endowment amounts from RV
+# Mint amounts to wallets
+# Deposit wallet amounts to 
+
+# OR
+
+
+# Start from a given block's aave state?
